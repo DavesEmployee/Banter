3a1a6f6e16778fbc3bd360b4877195174ab5730c
James Cook <davesemployee@gmail.com>
2023-07-27 18:03:34 -0400

    First commit

---
+++ OpenAIController.cs
@@ -0,0 +1,99 @@
+using TMPro;
+using OpenAI_API;
+using OpenAI_API.Chat;
+using OpenAI_API.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+// using Environment;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenAIController : MonoBehaviour
+{
+    public TMP_Text textField;
+    public TMP_InputField inputField;
+    public Button okButton;
+
+    private OpenAIAPI api;
+    private List<ChatMessage> messages;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        api = new OpenAIAPI(Environment.GetEnvironmentVariable("OPENAI_API_KEY", EnvironmentVariableTarget.User));
+        StartConversation();
+        okButton.onClick.AddListener(() => GetResponse());
+    }
+
+    private void StartConversation()
+    {
+        messages = new List<ChatMessage> {
+            new ChatMessage(ChatMessageRole.System, "You are an honorable gruff squid who holds a terrible secret. You will only allow someone you like to hear it. You exist on land and are a farmer")
+        };
+
+        inputField.text = "";
+        string startString = "You have just approached a funny looking farmer.";
+        textField.text = startString;
+        Debug.Log(startString);
+    }
+
+    private async void GetResponse()
+    {
+        if (inputField.text.Length < 1)
+        {
+            return;
+        }
+
+        // Disable the OK Button
+        okButton.enabled = false;
+
+        // Fill the user message from the input field
+        ChatMessage userMessage = new ChatMessage();
+        userMessage.Role = ChatMessageRole.User;
+        userMessage.Content = inputField.text;
+        if (userMessage.Content.Length > 100)
+        {
+            // Limit messages to 100 characters
+            userMessage.Content = userMessage.Content.Substring(0, 100);
+        }
+        Debug.Log(string.Format("{0}: {1}", userMessage.rawRole, userMessage.Content));
+
+        // Add the message to the list
+        messages.Add(userMessage);
+
+        // Update the text field with the user message
+        textField.text = string.Format("You: {0}", userMessage);
+
+        // Clear the input field
+        inputField.text = "";
+
+        // Send the entire chat to OpenAI to get the next message
+        var chatResult = await api.Chat.CreateChatCompletionAsync(new ChatRequest()
+        {
+            Model = Model.ChatGPTTurbo,
+            Temperature = 0.1,
+            MaxTokens = 50,
+            Messages = messages
+        });
+
+        // Get the response message
+        ChatMessage responseMessage = new ChatMessage();
+        responseMessage.Role = chatResult.Choices[0].Message.Role;
+        responseMessage.Content = chatResult.Choices[0].Message.Content;
+        Debug.Log(string.Format("{0}: {1}", responseMessage.rawRole, responseMessage.Content));
+
+        // Add the response to the list of messages
+        messages.Add(responseMessage);
+
+        // Update the text field with the response
+        textField.text = string.Format("You: {0}\n\nGuard: {1}", userMessage.Content, responseMessage.Content);
+
+        // Re-enable the OK button
+        okButton.enabled = true;
+
+
+    }
+}
