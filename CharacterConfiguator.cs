e97c15f2b6c47d1c1f297bf31790722cce4ba8db
James Cook <davesemployee@gmail.com>
2023-07-28 14:48:41 -0400

    Parameters

---
+++ CharacterConfiguator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterConfigurator : MonoBehaviour
+{
+    // public ModelType modelType;
+    public string name;
+    public string[] traits;
+    public int age;
+    public string background;
+    public string profession;
+    // public Dictionary<string, string> interactions;
+    // public Dictionary<string, int[]> feelings;
+    // public Dictionary<string, string> relationships;
+}
---
+++ OpenAIConfigurator.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenAIConfigurator : MonoBehaviour
+{
+    // public ModelType modelType;
+    public double temperature = 0.7;
+    public int maxTokens = 100;
+}
